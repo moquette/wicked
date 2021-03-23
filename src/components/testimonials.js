6cd6e017cdca5be09337e1cf1657f51d0307d34e
Joaquin A. Moquette <joaquin@moquette.us>
2021-03-23 16:40:30 -0700

    Glide slider

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,30 +1,67 @@
+import React, { useEffect } from "react";
+import Glide from "@glidejs/glide";
 export const Testimonials = (props) => {
   const data = props.data.testimonials;
+  useEffect(() => {
+    new Glide(".glide", {
+      type: "carousel",
+      perView: 2,
+      autoplay: 6000,
+      animationDuration: 1000,
+      breakpoints: {
+        768: {
+          perView: 1,
+        },
+        992: {
+          perView: 2,
+        },
+      },
+    }).mount();
+  });
   return (
     <section id={data.id} className={data.id + " " + data.classes}>
       <div className="container">
         <h2 className="section-title">{data.title}</h2>
-        <div className="row g-3 pt-3">
-          {data.quote.map((d, i) => (
-            <div
-              key={i}
-              className="col-md-4 pb-5"
-              data-aos="fade-up"
-              data-aos-delay={i * 150}
+
+        <div className="row g-3 pt-3 glide">
+          <div className="glide__track" data-glide-el="track">
+            <ul className="glide__slides">
+              {data.quote.map((d, i) => (
+                <li
+                  key={i}
+                  className="glide__slide"
+                  data-aos="fade-up"
+                  data-aos-delay={i * 150}
+                >
+                  <div className="testimonial-image">
+                    <img
+                      className="card-img-top rounded-circle"
+                      src={`./assets/images/testimonials/${d.image}`}
+                      alt={d.image}
+                    />
+                  </div>
+                  <div className="testimonial-content">
+                    <q>{d.text}</q>
+                    <p>~ {d.title}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="glide__arrows" data-glide-el="controls">
+            <button
+              className="glide__arrow glide__arrow--left"
+              data-glide-dir="<"
             >
-              <div className="testimonial-image">
-                <img
-                  className="card-img-top rounded-circle"
-                  src={`./assets/images/testimonials/${d.image}`}
-                  alt={d.image}
-                />
-              </div>
-              <div className="testimonial-content">
-                <q>{d.text}</q>
-                <p>~ {d.title}</p>
-              </div>
-            </div>
-          ))}
+              <i className="bi bi-arrow-left-short"></i>
+            </button>
+            <button
+              className="glide__arrow glide__arrow--right"
+              data-glide-dir=">"
+            >
+              <i className="bi bi-arrow-right-short"></i>
+            </button>
+          </div>
         </div>
       </div>
     </section>
